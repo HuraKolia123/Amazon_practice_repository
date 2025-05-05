873d4733bbd1b841ad258d027d7862b4ddda960d
HuraKolia123 <kolyagura569@ukr.net>
2025-05-05 18:48:47 +0300

    there were problems with viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import svgr from "vite-plugin-svgr";
+
+// export default defineConfig({
+//   plugins: [react(), svgr()],
+//   resolve: {
+//     alias: [{ find: "@", replacement: "/src" }],
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import path from "path"; // ✅ потрібен для резолву
 
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "src"), // ✅ абсолютний шлях до src
+      },
+    ],
   },
 });
