8394306e65cf95aa9dc43b7691edff57a4017d72
Hura123 <kolyagura569@ukr.net>
2025-05-08 18:00:10 +0300

    ./.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import svgr from "vite-plugin-svgr";
-
-// export default defineConfig({
-//   plugins: [react(), svgr()],
-//   resolve: {
-//     alias: [{ find: "@", replacement: "/src" }],
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import tsconfigPaths from "vite-tsconfig-paths"; // 👈
 
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths()], // 👈 підключає alias-и з tsconfig
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
 });
